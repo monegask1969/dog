663efdf7bc4dcc784225e30aa0a06b55b9ec48c9
Максим Сафронов < >
2016-12-27 20:46:41 +0200

    Initial commit

---
+++ src/main/java/com/dip/DogApplication.java
@@ -0,0 +1,12 @@
+package com.dip;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DogApplication.class, args);
+	}
+}
